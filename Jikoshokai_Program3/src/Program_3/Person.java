21e2cd986d3ebf1f4a52cd58323932407dabf2aa
s-miyachi <miyachi1031@gmail.com>
2024-06-29 20:49:56 +0900

    課題1.32　新規作成　提出

---
+++ Jikoshokai_Program3/src/Program_3/Person.java
@@ -0,0 +1,54 @@
+package Program_3;
+
+public class Person{
+	
+	public static int count = 0;
+	public String firstName;
+	public int age;
+	public double height, weight;
+	
+	// 問１　
+	public String lastName;
+	
+	Person(String firstName, int age, double height, double weight){
+		
+	this.firstName = firstName;
+	this.age = age;		
+	this.height = height;
+	this.weight = weight;
+	
+	}
+	
+	// 問２
+	Person(String firstName, String lastName, int age, double height, double weight){
+			
+		// 問３
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;		
+		this.height = height;
+		this.weight = weight;
+		Person.count++;
+		
+	}
+
+	public String fullName(){
+	
+		return this.firstName + this.lastName;
+	
+	}
+	
+	public void print(){
+	System.out.println("名前は" + this.fullName() + "です");
+	System.out.println("年は" + this.age + "です");
+	}
+	
+	public double bmi(){
+	return this.weight / this.height / this.height;
+	}
+	
+	public static void printCount(){
+	System.out.println("合計" + Person.count + "人です");
+	
+	}
+}
